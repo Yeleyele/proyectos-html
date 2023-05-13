cf4c13782af1c2d3502c78e15a4678798f2fc7a1
Yeleyele <131538501+Yeleyele@users.noreply.github.com>
2023-05-13 10:01:20 -0500

    Add files via upload

---
+++ SEMANA 15/JS.js
@@ -0,0 +1,6 @@
+function saludo(nombre){
+
+    alert(“Hola…”+nombre +" " + "Vamos aprender los 5 programas fantásticos:"+"\n"+"HTML- CSS- JS – PHP-MYSQL");
+    
+    }
+    
---
+++ SEMANA 15/js 3.js
@@ -0,0 +1,5 @@
+function vista_miniatura (ruta){
+
+    document.getElementById("visor_great").src = ruta
+    
+    }
